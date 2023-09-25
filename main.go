7b8a2b1377b6f67b4cb0cb5b28618731466b6cf9
nomuyoshi <nomuyoshi0111@gmail.com>
2023-09-25 09:39:19 +0900

    use default RateLimiterMemoryStore

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(1)))
 	e.GET("/hello", func(c echo.Context) error {
 		res := struct {
 			Message string
@@ -17,5 +20,12 @@ func main() {
 		return c.JSON(http.StatusOK, res)
 	})
 
-	e.Logger.Fatal(e.Start(":3000"))
+	s := http.Server{
+		Addr:    ":3000",
+		Handler: e,
+	}
+
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
